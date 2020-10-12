ef5d00e903a3aadd6832c894c6579fba8b054cc3
dxdevoe <dxdevoe@users.noreply.github.com>
2020-10-12 15:02:55 -0400

    Update stepper.py

--- stepper.py
+++ stepper.py
@@ -3,56 +3,35 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-currentAngle = 0
-minDelayTime = 1000
-seq = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
-        [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
-seq.reverse()
-currentState = 0
-
 pins = [14,15,18,23] # controller inputs: in1, in2, in3, in4
 for pin in pins:
   GPIO.setup(pin, GPIO.OUT, initial=0)
+
+# Define the pin sequence for counter-clockwise motion, noting that
+# two adjacent phases must be actuated together before stepping to
+# a new phase so that the rotor is pulled in the right direction:
+ccw = [ [1,0,0,0],[1,1,0,0],[0,1,0,0],[0,1,1,0],
+        [0,0,1,0],[0,0,1,1],[0,0,0,1],[1,0,0,1] ]
+# Make a copy of the ccw sequence. This is needed since simply
+# saying cw = ccw would point both variables to the same list object:
+cw = ccw[:]  # use slicing to copy list (could also use ccw.copy() in Python 3)
+cw.reverse() # reverse the new cw sequence
 
 def delay_us(tus): # use microseconds to improve time resolution
   endTime = time.time() + float(tus)/ float(1E6)
   while time.time() < endTime:
     pass
 
-def sgn(val):   # signum function
-  return(int(val/abs(val)))
-
-def step(dir):
-  global currentState
-  currentState += dir
-  if currentState > 7:
-    currentState = 0
-  elif currentState < 0:
-    currentState = 7
-  for pin in range(4):
-    GPIO.output(pins[pin], seq[currentState][pin])
-
-def goAngle(newAngle, speed):
-  global currentAngle
-  deltaAngle = newAngle - currentAngle
-  if abs(deltaAngle) > 180:
-    deltaAngle = (abs(deltaAngle)-180)*(-sgn(deltaAngle))
-  if speed > 1:
-    speed = 1
-  elif speed <= 0:
-    speed = 0.05
-  # 512 cycle/rev * 1/360 rev/degree * 8 step/cycle = 11.378 step/degree
-  stepsPerDegree = 11.378
-  numSteps = abs(int(stepsPerDegree * deltaAngle))
-  direction = sgn(deltaAngle)
-  for s in range(numSteps):
-    step(direction)
-    delay_us(minDelayTime/speed)
-  currentAngle = newAngle
-
+# Make a full rotation of the output shaft:
+def loop(dir): # dir = rotation direction (cw or ccw)
+  for i in range(512): # full revolution (8 cycles/rotation * 64 gear ratio)
+    for halfstep in range(8): # 8 half-steps per cycle
+      for pin in range(4):    # 4 pins that need to be energized
+        GPIO.output(pins[pin], dir[halfstep][pin])
+      delay_us(1000)
 try:
-  goAngle(90, 0.8)   # (angle, fractional speed)
-  time.sleep(1)
-except Exception as e:
-  print(e)
-GPIO.cleanup()
+  loop(cw)
+  loop(ccw)
+except:
+  pass
+GPIO.cleanup() 
