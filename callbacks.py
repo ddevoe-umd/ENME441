bbf8f185f3481137ce31462bd88b7d5283a5bce7
dxdevoe <dxdevoe@users.noreply.github.com>
2021-09-27 12:46:43 -0400

    Update callbacks.py

--- callbacks.py
+++ callbacks.py
@@ -1,21 +1,22 @@
 import RPi.GPIO as gpio
+import time
 
 # Define input port numbers:
 in1, in2 = 17, 27
-
 gpio.setmode(gpio.BCM)
 gpio.setup(in1, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 gpio.setup(in2, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
 # Define a threaded callback function:
-def myCallback(channel):
-  print("Rising edge detected on pin %d" % channel)
-
+def myCallback(pin):
+  print("Rising edge detected on pin %d" % pin)
+  
 # Execute myCallback() if port 1 goes HIGH:
 gpio.add_event_detect(in1, gpio.RISING, callback=myCallback, bouncetime=100)
 
-# pause main code until port 2 goes LOW:
-gpio.wait_for_edge(in2, gpio.FALLING)
-print("Falling edge detected on port", str(in2))
+# Infinite loop:
+while True:
+  print('.', end='')
+  time.sleep(0.1)
 
 gpio.cleanup()
